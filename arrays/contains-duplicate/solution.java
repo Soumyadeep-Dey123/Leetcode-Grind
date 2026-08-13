6a5af7c83f8c3f9bc3be80662adcd28cba8e9cce
SoumyadeepDey <dey.soumyadeep123@gmail.com>
2026-08-13 16:17:53 +0530

    Solution drafted, works like a charm. Leetcode#217 solved

--- arrays/contains-duplicate/solution.java
+++ arrays/contains-duplicate/solution.java
@@ -1,7 +1,25 @@
+import java.util.HashSet;
 
 class solution {
     public boolean containsDuplicate(int[] nums) {
-        
-        return true;
+        HashSet<Integer> uniqueVals = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!uniqueVals.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        solution sol = new solution();
+
+        int[] test1 = { 1, 2, 3, 1 };
+        int[] test2 = { 1, 2, 3, 4 };
+        int[] test3 = {};
+
+        System.out.println("test1 -> " + sol.containsDuplicate(test1)); // true
+        System.out.println("test2 -> " + sol.containsDuplicate(test2)); // false
+        System.out.println("test3 -> " + sol.containsDuplicate(test3)); // false
     }
 }
